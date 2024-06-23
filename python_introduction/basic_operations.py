a404a828a3aa5d4d6cadfb0c23004b7bda5b130c
Paulose <pthiopia@outlook.com>
2024-06-23 20:25:36 +0300

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+"number1 = 10"
+"number2 = 5"
+print ("number1 + number2")
+print ("number1 - number2")
+print ("number1 * number2")
+Sum= 15
+Difference= 5
+Product= 50
+print("[Addition] of [number1] and [number2] is [sum]")
+print("[Subtruction] of [number1] and [number2] is [Difference]")
+print("[Multiplication] of [number1] and [number2] is [Product]")
