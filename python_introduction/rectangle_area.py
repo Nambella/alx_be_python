d7f13ad4fd376697c1cfe628ba6dfc74ef3e0062
Paulose <pthiopia@outlook.com>
2024-06-23 21:44:09 +0300

    rectangle_area.py, simple_interest.py

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,4 +1,4 @@
 length = 10 
 width = 5
-area = 50 
+area = length * width  
 print("[multiplication] of [length] and [width] is [area]")
--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,4 @@ principal = 1000
 rate = 0.05
 time = 3
 interest = 150.0
-print("[multiplication] of [principal] and [rate] and [time] is [simple_interest]")
 print( "smple_interest:", interest)
