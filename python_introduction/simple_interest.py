80cb16894a93f98de3ab7b605e8d406df32666f4
Paulose <pthiopia@outlook.com>
2024-06-23 21:04:24 +0300

    simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
 principal = 1000
 rate = 0.05
 time = 3
-simple_interest = (principal * rate * time)
+interest= (principal * rate * time)
 interest = 150.0
 print(["simple_interest:"] is "[interest]")
