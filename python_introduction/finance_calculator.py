928246dd82abfad0d9cda1a0d13d5622eb24fbb7
Paulose <pthiopia@outlook.com>
2024-06-24 01:46:24 +0300

    financial_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
-Monthly_income = 5000
-Monthly_expenses = 4000
-Monthly_savings = (5000 - 4000)
-Interest = (1000 * 12 * 0.05)
+Monthly_income = int(input("Enter your monthly income"))
+Monthly_expenses = int(input("Enter your monthly expenses"))
+Monthly_savings = (Monthly_income - Monthly_expenses)
+Interest = (Monthly_savings * 12 * 0.05)
 Projected_savings = (Monthly_savings * 12 + (Monthly_savings * 12 * 0.05))
