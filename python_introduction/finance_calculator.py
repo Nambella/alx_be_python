ec80c928b66a1d40f20858b67777fad5f8b28a06
Paulose Duressa <pthiopia@outlook.com>
2024-06-24 03:48:44 +0300

    Delete python_introduction/finance_calculator.py

--- python_introduction/finance_calculator.py
+++
@@ -1,6 +0,0 @@
-Monthly income = int(input("Enter your monthly income:"))
-Monthly expenses = int(input("Enter your monthly expenses:"))
-Monthly savings = Monthly income - Monthly expenses
-Interest = (Monthly savings * 12 * 0.05)
-Projected savings = (Monthly savings * 12 + (Monthly savings * 12 * 0.05))
-print(f"projected saving after one year: {projected savings}")
