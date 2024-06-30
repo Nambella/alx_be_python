5e81102bfef742642f1959baca99992c2711f7cb
Paulose <pthiopia@outlook.com>
2024-06-30 18:22:50 +0300

    match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,14 +2,14 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 result = None
-if operation == "+": 
+if operation == "+" : 
     result = num1 + num2
-elif operation == "-": 
+elif operation == "-" : 
     result = num1 - num2
-elif operation == "*":
+elif operation == "*" :
     result = num1 * num2
-elif operation == "/": 
-    if num2 != 0:
+elif operation == "/" : 
+    if num2 != 0 :
         result = num1 / num2
     else :
         print("Error: Division by zero is not allowed.")
