2c09d4af3d3bf0f3734ca33828575f708ce9edbc
Paulose <pthiopia@outlook.com>
2024-07-06 22:26:01 +0300

    arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -22,7 +22,7 @@ def perform_operation(num1, num2, operation):
         if num2 != 0:
             return num1 / num2
         else:
-            return "Error: Division by zero"
+            return "Error: divided by zero"
 
 if __name__ == "__main__":
     result = perform_operation(10, 5, 'add')
