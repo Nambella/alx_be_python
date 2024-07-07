835c22bc67c54a89e3797c13c024acc171d13ce7
Paulose <pthiopia@outlook.com>
2024-07-07 08:36:11 +0300

    explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,20 +1,20 @@
-import datetime
+from datetime import datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time: {formatted_date}")
 
-display_current_datetime()
 def calculate_future_date():
     try:
         num_days = int(input("Enter the number of days to add: "))
-        current_date = datetime.datetime.now()
-        future_date = current_date + datetime.timedelta(days=num_days)
+        future_date = datetime.now() + timedelta(days=num_days)
         formatted_future_date = future_date.strftime("%Y-%m-%d")
         print(f"Future date after {num_days} days: {formatted_future_date}")
     except ValueError:
         print("Invalid input. Please enter a valid integer.")
 
-calculate_future_date()
+if __name__ == "__main__":
+    display_current_datetime()
+    calculate_future_date()
 
