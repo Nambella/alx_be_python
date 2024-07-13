93b94850605e0ab338a89fbe4e1b117d49d3c5a6
Paulose <pthiopia@outlook.com>
2024-07-13 19:07:11 +0300

    test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,25 +1,29 @@
 import unittest
 from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
-    def test_add(self):
-     calc = SimpleCalculator()
-     self.assertEqual(calc.add(2, 3), 5)
-     self.assertEqual(calc.add(-1, 1), 0)
-     self.assertEqual(calc.add(-1, -1), -2)
+
+    def test_addition(self):
+        calc = SimpleCalculator()
+        self.assertEqual(calc.add(2, 3), 5)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-1, -1), -2)
+ 
     def test_subtract(self):
-     calc = SimpleCalculator()
-     self.assertEqual(calc.subtract(10, 5), 5)
-     self.assertEqual(calc.subtract(-1, 1), -2)
-     self.assertEqual(calc.subtract(-1, -1), 0)
+        calc = SimpleCalculator()
+        self.assertEqual(calc.subtract(10, 5), 5)
+        self.assertEqual(calc.subtract(-1, 1), -2)
+        self.assertEqual(calc.subtract(-1, -1), 0)
+
     def test_multiply(self):
-     calc = SimpleCalculator()
-     self.assertEqual(calc.multiply(3, 7), 21)
-     self.assertEqual(calc.multiply(-1, 1), -1)
-     self.assertEqual(calc.multiply(-1, -1), 1)
+        calc = SimpleCalculator()
+        self.assertEqual(calc.multiply(3, 7), 21)
+        self.assertEqual(calc.multiply(-1, 1), -1)
+        self.assertEqual(calc.multiply(-1, -1), 1)
+
     def test_divide(self):
-     calc = SimpleCalculator()
-     self.assertEqual(calc.divide(10, 2), 5)
-     self.assertEqual(calc.divide(-1, 1), -1)
-     self.assertEqual(calc.divide(-1, -1), 1)
-     with self.assertRaises(ValueError):
-        calc.divide(10, 0)
+        calc = SimpleCalculator()
+        self.assertEqual(calc.divide(10, 2), 5)
+        self.assertEqual(calc.divide(-1, 1), -1)
+        self.assertEqual(calc.divide(-1, -1), 1)
+        with self.assertRaises(ValueError):
+            calc.divide(10, 0)
