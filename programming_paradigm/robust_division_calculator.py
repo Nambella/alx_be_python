a91ab8822966b72fccd6769287feebee3b4f353e
Paulose <pthiopia@outlook.com>
2024-07-19 14:18:52 +0300

    robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,9 +3,14 @@ def safe_divide(numerator, denominator):
         numerator = float(numerator)
         denominator = float(denominator)
         result = numerator / denominator
+        return result
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
-    return result
+
+if __name__ == "__main__":
+    num = input("Enter the numerator: ")
+    denom = input("Enter the denominator: ")
+    print(safe_divide(num, denom))
 
