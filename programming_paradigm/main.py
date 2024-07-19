9039cd95907b0ef661f9e9c829a5f94b7b20397e
Paulose <pthiopia@outlook.com>
2024-07-19 14:39:11 +0300

    main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,13 +1,16 @@
-from robust_division_calculator import safe_divide
+# main.py
+
 import sys
+from robust_division_calculator import safe_divide
+
 def main():
     if len(sys.argv) != 3:
         print("Usage: python main.py <numerator> <denominator>")
-    else:
-        numerator = sys.argv[1]
-        denominator = sys.argv[2]
-        result = safe_divide(numerator, denominator)
-    print(f"Result: {result}")
+        sys.exit(1)
+
+    numerator, denominator = sys.argv[1], sys.argv[2]
+    result = safe_divide(numerator, denominator)
+    print(f"The result of the division is {result:.2f}")
 
 if __name__ == "__main__":
-    main ()
+    main()
