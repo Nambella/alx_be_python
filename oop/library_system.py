7d553bd0e7ee277428fb0ee5915dfd0c1d0d3c65
Paulose <pthiopia@outlook.com>
2024-07-28 22:02:58 +0300

    library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -39,6 +39,7 @@ if __name__ == "__main__":
     classic_book = Book ("Pride and Prejudice", "Jane Austen")
     ebook1 = EBook("Snow Crash", "Neal Stephenson", 500)
     printbook1 = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+    library.add_book(classic_book)
     library.add_book(ebook1)
     library.add_book(printbook1)
     library.list_books()
